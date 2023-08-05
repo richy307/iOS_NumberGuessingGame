a65bea7d24cf09c9504bd169a422595f924105ce
Richy <richy@webcomm.com.tw>
2023-08-05 21:30:18 +0800

    Initial Commit

---
+++ iOS_NumberGuessingGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS_NumberGuessingGame
+//
+//  Created by 王麒翔 on 2023/8/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
