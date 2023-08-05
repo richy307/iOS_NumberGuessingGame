ab515273f7e313741de53327f25068e8dd0efc83
Richy <richy@webcomm.com.tw>
2023-08-06 00:52:56 +0800

    new function checkAB

--- iOS_NumberGuessingGame/Basic.swift
+++ iOS_NumberGuessingGame/Basic.swift
@@ -37,4 +37,31 @@ class Basic {
         return true
     }
     
+    // 檢查幾A幾B
+    func checkAB(test: String, answer: String) -> (A:Int, B:Int)? {
+        
+        if isOKNumber(input: test) == false || isOKNumber(input: answer) == false {
+            return nil
+        }
+        
+        var resultA = 0
+        var resultB = 0
+        let testArray = Array(test)
+        let answerArray = Array(answer)
+        
+        for i in 0 ..< MAX_NUMBER {
+            for j in 0 ..< MAX_NUMBER {
+                
+                if testArray[i] == answerArray[j] && i == j {
+                    // A：數字相同、位置相同
+                    resultA += 1
+                } else if testArray[i] == answerArray[j] {
+                    // B：數字相同、位置不同
+                    resultB += 1
+                }
+            }
+        }
+        return (A:resultA, B:resultB)
+    }
+    
 }
