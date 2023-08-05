e6de2ea6992915d73a2431c54280c4ff593c542e
Richy <richy@webcomm.com.tw>
2023-08-06 02:35:22 +0800

    new function generateAnswerNumberString, init()

--- iOS_NumberGuessingGame/Computer.swift
+++ iOS_NumberGuessingGame/Computer.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class Computer: Basic {
+    
+    var answerUmberString = ""
+    
+    func generateAnswerNumberString() {
+        // 產生答案
+        var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+        
+        for _ in 1 ... MAX_NUMBER {
+            let index = Int.random(in: 0 ..< numbers.count)
+            let number = numbers[index]
+            answerUmberString += number
+            
+            if ALLOW_DUPLICATE_NUMBER == false {
+                numbers.remove(at: index)
+            }
+        }
+    }
+    
+    override init() {
+        super.init()
+        generateAnswerNumberString()
+    }
+}
