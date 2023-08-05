27b2d65d5d7cb73cbb08c9e8a1151012ed089fc5
Richy <richy@webcomm.com.tw>
2023-08-05 22:52:27 +0800

    connect IBOutlet, IBAction

--- iOS_NumberGuessingGame/ViewController.swift
+++ iOS_NumberGuessingGame/ViewController.swift
@@ -8,7 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var guessTextField: UITextField!
+    @IBOutlet weak var inputA: UITextField!
+    @IBOutlet weak var inputB: UITextField!
+    @IBOutlet weak var playerLog: UITextView!
+    @IBOutlet weak var computerLog: UITextView!
+    
+    @IBAction func rest(_ sender: UIBarButtonItem) {
+    }
+    @IBAction func guess(_ sender: UIButton) {
+    }
+    @IBAction func reply(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
