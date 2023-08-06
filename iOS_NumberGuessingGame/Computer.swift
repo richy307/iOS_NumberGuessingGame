db619af91d802769b8e7988e884beff386e04c97
Richy <richy@webcomm.com.tw>
2023-08-06 14:16:33 +0800

    調整參數名稱

--- iOS_NumberGuessingGame/Computer.swift
+++ iOS_NumberGuessingGame/Computer.swift
@@ -9,7 +9,7 @@ import Foundation
 
 class Computer: Basic {
     
-    var answerUmberString = ""
+    var answerNumberString = ""
     var guessCount = 0
     
     func generateAnswerNumberString() {
@@ -19,16 +19,17 @@ class Computer: Basic {
         for _ in 1 ... MAX_NUMBER {
             let index = Int.random(in: 0 ..< numbers.count)
             let number = numbers[index]
-            answerUmberString += number
+            answerNumberString += number
             
             if ALLOW_DUPLICATE_NUMBER == false {
                 numbers.remove(at: index)
             }
         }
+        print("==== Answer:\(answerNumberString)") // 答案
     }
     
     func userGuess(input: String) -> (A:Int, B:Int)? {
-        let result = checkAB(test: input, answer: answerUmberString)
+        let result = checkAB(test: input, answer: answerNumberString)
         if result != nil {
             guessCount += 1
         }
